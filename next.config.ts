63a45dd0eae51fe744d835ec971dafeb6cb2b1e5
theo.vambre <theo_vambre@ens.univ-artois.fr>
2024-12-06 14:16:16 +0100

    suite

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    assetPrefix: './', // Nécessaire pour GitHub Pages
+    images: {
+        unoptimized: true, // Optimisation désactivée pour faciliter GitHub Pages
+    },
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
